b45b49f0745f7daeae5221fc61c2fb849958806c
toghrulvg <toghrulvg@code.edu.az>
2022-06-22 06:18:23 +0400

    task

---
+++ BackReading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    namespace homework
+    {
+        class Program
+        {
+            static void Main(String[] args)
+            {
+            BackReading("Togrul");
+            BackReading("ey melek kelem ye");
+            }
+            
+            static void BackReading(string text)
+            {
+                string backReading = string.Empty;
+                
+                for (int i=text.Length - 1; i>= 0; i--)
+                {
+                    backReading += text[i];
+                }
+            Console.WriteLine($"BackReading text is : {backReading} ");
+            }   
+        }
+
+            
+    }
+        
